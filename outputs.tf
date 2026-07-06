bd3659326de458e9ded84da5c64589da1b3e9dc1
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "route_server_bgp_connections" {
   description = "All route_server_bgp_connection resources"
   value       = azurerm_route_server_bgp_connection.route_server_bgp_connections
 }
+output "route_server_bgp_connections_name" {
+  description = "List of name values across all route_server_bgp_connections"
+  value       = [for k, v in azurerm_route_server_bgp_connection.route_server_bgp_connections : v.name]
+}
+output "route_server_bgp_connections_peer_asn" {
+  description = "List of peer_asn values across all route_server_bgp_connections"
+  value       = [for k, v in azurerm_route_server_bgp_connection.route_server_bgp_connections : v.peer_asn]
+}
+output "route_server_bgp_connections_peer_ip" {
+  description = "List of peer_ip values across all route_server_bgp_connections"
+  value       = [for k, v in azurerm_route_server_bgp_connection.route_server_bgp_connections : v.peer_ip]
+}
+output "route_server_bgp_connections_route_server_id" {
+  description = "List of route_server_id values across all route_server_bgp_connections"
+  value       = [for k, v in azurerm_route_server_bgp_connection.route_server_bgp_connections : v.route_server_id]
+}
 
